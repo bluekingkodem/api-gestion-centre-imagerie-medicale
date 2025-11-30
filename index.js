194cf12ad90ae5ca6fe1fb761f08ef0f9fdb2a1e
kodem <obamambia@gmail.com>
2025-11-30 19:03:47 +0100

    fin configuration du serveur express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Hello Express !");
+});
+
+app.listen(port, () => console.log(`Server listening on ${port}`));
